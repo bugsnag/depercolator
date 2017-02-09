2dffa66e436977fdb9870a69842c59788b9f4b51
Christian Schlensker <christian@cswebartisan.com>
2017-02-08 17:04:43 -0800

    add create-element-to-jsx codemod and prettier

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+var path = require('path');
 var program = require('commander');
 var shell = require('shelljs');
 var resolveBin = require('resolve-bin');
@@ -7,11 +8,10 @@ var resolveBin = require('resolve-bin');
 var decafPath = resolveBin.sync('decaffeinate');
 var prettierPath = resolveBin.sync('prettier');
 var cjsxTransformPath = resolveBin.sync('coffee-react-transform', { executable: 'cjsx-transform' });
-
+var rceToJSXPath = path.resolve(__dirname,'node_modules/react-codemod/transforms/create-element-to-jsx.js');
 
 shell.config.silent = true;
 shell.config.fatal = true;
-
 
 function decaffeinateCommand() {
   var command = [ decafPath ];
@@ -27,6 +27,10 @@ function cjsxTransformCommand(file) {
   return [ cjsxTransformPath, file ].join(' ');
 }
 
+function jsCodeShiftCommand(file) {
+  return ['jscodeshift -t', rceToJSXPath, file].join(' ');
+}
+
 function makeOutput(file) {
   return file.replace(/.cjsx$/, '.jsx').replace(/.coffee$/, '.js');
 }
@@ -36,8 +40,13 @@ function processFile(file) {
   var result = shell
     .exec(cjsxTransformCommand(file))
     .exec(decaffeinateCommand())
-    .exec(prettierPath)
     .to(output);
+
+  // convert React.createElement to jsx
+  shell.exec(jsCodeShiftCommand(output));
+
+  // prettier
+  shell.exec(prettierPath + ' --write ' + output);
 
   console.log('Converted ' + file + ' → ' + output)
 }
