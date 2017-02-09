d74fad440038472446fee899005ea7997fe03c5e
Christian Schlensker <christian@cswebartisan.com>
2017-02-08 22:06:11 -0800

    add eslint option, fix jscodeshift and react-codemod

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
+var chalk = require('chalk');
 var path = require('path');
 var program = require('commander');
 var shell = require('shelljs');
 var resolveBin = require('resolve-bin');
 
-
 var decafPath = resolveBin.sync('decaffeinate');
 var prettierPath = resolveBin.sync('prettier');
+var jscodeshiftPath = resolveBin.sync('jscodeshift');
 var cjsxTransformPath = resolveBin.sync('coffee-react-transform', { executable: 'cjsx-transform' });
-var rceToJSXPath = path.resolve(__dirname,'node_modules/react-codemod/transforms/create-element-to-jsx.js');
+var rceToJSXPath = path.resolve(__dirname,'./node_modules/react-codemod/transforms/create-element-to-jsx.js');
 
 shell.config.silent = true;
 shell.config.fatal = true;
@@ -28,16 +29,26 @@ function cjsxTransformCommand(file) {
 }
 
 function jsCodeShiftCommand(file) {
-  return ['jscodeshift -t', rceToJSXPath, file].join(' ');
+  return [jscodeshiftPath, '-t', rceToJSXPath, file].join(' ');
 }
 
 function makeOutput(file) {
   return file.replace(/.cjsx$/, '.jsx').replace(/.coffee$/, '.js');
 }
 
+function renderError(message) {
+  console.log("\n");
+  console.log(chalk.bgRed('ERROR:') + ' ' + chalk.red(message));
+}
+
+function renderSuccess(message) {
+  console.log("\n");
+  console.log(chalk.bgGreen.black.bold('DONE:') + ' ' + chalk.green(message));
+}
+
 function processFile(file) {
   var output = program.output || makeOutput(file);
-  var result = shell
+  shell
     .exec(cjsxTransformCommand(file))
     .exec(decaffeinateCommand())
     .to(output);
@@ -48,12 +59,21 @@ function processFile(file) {
   // prettier
   shell.exec(prettierPath + ' --write ' + output);
 
-  console.log('Converted ' + file + ' → ' + output)
+  if (program.eslintFix) {
+    if (!shell.which('eslint')) {
+      renderError('eslint must be present when specifying --eslint-fix');
+      shell.exit(1);
+    }
+    shell.exec('eslint --fix ' + output)
+  }
+
+  renderSuccess('Converted ' + file + chalk.bold.white(' → ') + output);
 }
 
 program
   .arguments('<file>')
   .option('--prefer-const', 'Use "const" when possible in output code')
   .option('-o, --output [filepath]', 'Output file path')
+  .option('-e, --eslint-fix', 'Perform eslint --fix on resulting file')
   .action(processFile)
   .parse(process.argv);
