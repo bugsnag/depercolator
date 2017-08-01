2d9e06429909c2496d1a3f9481f417242722d439
Mikael Carpenter <mikael@guidebook.com>
2017-08-01 11:50:56 -0700

    closes #20 . Remove <int> before camelCasing. parseInt on value if needed

--- src/index.js
+++ src/index.js
@@ -43,8 +43,12 @@ const prettierOptions = [
 function getOptions(array) {
   const options = {};
   array.forEach(([flag]) => {
-    const key = camelCase(flag);
-    options[key] = program[key];
+    const key = camelCase(flag.replace(' <int>', ''));
+    let value = program[key];
+    if(typeof value === 'string') {
+      value = parseInt(value)
+    }
+    options[key] = value;
   });
 
   return options;
